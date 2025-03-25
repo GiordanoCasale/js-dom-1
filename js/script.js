469d4360f0c7908beda1fa2bdd27274473da9c8b
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-25 14:08:01 +0100

    [UPD] Variable created

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//inizializziamo le variabili
+const lamp = document.getElementById("lamp");
+const interruttore = document.getElementById("btn-on-off");
+
+//aggiungiamo la funzione click al bottone
